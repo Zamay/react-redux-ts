d1131bac3b36e16d1f015855cee90154ab9bb633
Mitya Zamay <mityazamay@gmail.com>
2022-02-23 17:15:06 +0200

    commit

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react';
+import {FC, useEffect} from 'react';
 import {useTypedSelector} from "../hooks/useTypedSelector";
 import {fetchUsers} from "../store/action-creators/user";
 import {useActions} from "../hooks/useActions";
 
-const UserList: React.FC = () => {
+const UserList: FC = () => {
     const {users, error, loading} = useTypedSelector(state => state.user)
     const {fetchUsers} = useActions()
 
